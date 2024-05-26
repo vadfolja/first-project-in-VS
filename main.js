ed9a7c5e9f074463cc483c3c12f3bca386532287
vadfolja <vadikapple02@gmail.com>
2024-05-26 10:56:58 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let section_item_github = document.querySelector('.section-item_github')
+$('section_item').on('click', function(event) {
+    window.location.href = 'https://github.com/vadfolja';
+});
+
+let section_item_instagram = document.querySelector('.section-item_instagram')
+$('section_item').on('click', function(event) {
+    window.location.href = 'https://www.instagram.com/vadfolja/';
+});
